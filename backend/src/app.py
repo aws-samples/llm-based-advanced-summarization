c4577419f9cbe81744e4c73ec8f094ee572938b8
Tanner McRae <tannermcrae@gmail.com>
2024-01-12 16:59:23 -0800

    Adding multi-doc support and API to upload documents

--- backend/src/app.py
+++ backend/src/app.py
@@ -5,10 +5,24 @@ from botocore.config import Config
 from technique.stuff_it import StuffIt
 from technique.map_reduce import MapReduce
 from technique.auto_refine import AutoRefine
+from technique.multi_doc import MultiDoc
 from type.step import Step
 import time
+import pickle
+import os
+import uuid
+
+UPLOAD_FOLDER = 'documents'
+# This demo only allows txt files. You could upload anything you want, you just have to normalize
+# it into text first.
+ALLOWED_EXTENSIONS = {'txt'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Every time you startup the server, delete the old documents folder and recreate it.
+if not os.path.exists(UPLOAD_FOLDER):
+  os.makedirs(UPLOAD_FOLDER)
 
 # Increase the standard time out limits in boto3, because Bedrock may take a while to respond to large requests.
 my_config = Config(connect_timeout=60*3, read_timeout=60*3)
@@ -17,6 +31,7 @@ bedrock_client = boto3.client(service_name='bedrock-runtime', config=my_config)
 # Create the different summarization clients to be used.
 stuff_it_client: StuffIt = StuffIt(bedrock_client)
 map_reduce_client: MapReduce = MapReduce(bedrock_client)
+multi_doc_client: MultiDoc = MultiDoc(bedrock_client)
 
 @app.route('/stuff-it',  methods=['POST'])
 def stuff_it():
@@ -50,6 +65,45 @@ def auto_refine():
   results: dict = auto_refine_client.auto_refine(doc, prompt_options, AUTO_REFINE=True)
   return jsonify(_format_results(results))
 
+@app.route('/multi-doc', methods=['POST'])
+def multi_doc():
+  data: dict = request.json
+  file_location: list[str] = data['uploadLocation']
+  description_of_documents: str = data['descriptionOfDocuments']
+  questions_about_docs: list[str] = data['questionsAboutDocs']
+
+  docs: list[str] = _get_docs_from_filepath(file_location)
+  results: dict = multi_doc_client.multi_doc(docs, questions_about_docs, description_of_documents)
+
+  return jsonify(_format_results(results))
+
+#####
+# Reads in files from uploaded doc list, pickles them and then 
+# returns the path to the file to be used in subsequent calls.
+#####
+@app.route('/upload-docs', methods=['POST'])
+def upload_many_docs():
+  if 'file' not in request.files:
+    return jsonify({ 'error': 'No file uploaded' })
+  
+  files = request.files.getlist("file")
+  
+  documents: list[str] = []
+
+  for file in files:
+    if file.filename == '':
+      return jsonify({ 'error': 'No file selected' })
+    
+    if file: 
+      text_data: str = file.read().decode('utf-8')
+      documents.append(text_data)
+  
+  result_path: str = _write_docs_to_file(documents)
+  return jsonify({ 'file_path': result_path })
+
+
+
+
 ########################
 # Helper functions
 ########################
@@ -67,3 +121,17 @@ def _wrap_in_duration(doc: str, func: callable) -> dict:
   end_time = time.time()
   results['time'] = round(end_time - start_time, 2)
   return results
+
+def _get_docs_from_filepath(path: str) -> list[str]:
+  docs: list[str] = []
+  with open(path, 'rb') as file:
+    docs = pickle.load(file)
+  return docs
+
+def _write_docs_to_file(docs: list[str]) -> str:
+  # Define a unique name for the pickled files.
+  filename = str(uuid.uuid4()) + '.pkl'
+  path = f'{UPLOAD_FOLDER}/{filename}'
+  with open(path, 'wb') as f:
+    pickle.dump(docs, f)
+  return path
---
+++ backend/src/technique/multi_doc.py
@@ -0,0 +1,139 @@
+import boto3
+from technique.base_advanced_summarization import BaseAdvancedSummarization
+from type.step import Step
+import time
+
+# Property of Claude 2
+MAX_TOKEN_COUNT = 12000
+# How many times to retry if Claude is not working.
+MAX_ATTEMPTS = 30 
+
+class MultiDoc(BaseAdvancedSummarization):
+
+  def __init__(self, client: boto3.client, cache_responses: bool=False, debug: bool = False):
+    super().__init__(client)
+    self.cache_responses = cache_responses
+    self.debug = debug
+    self.steps: list[Step] = []
+
+  # This function uses the three helper functions to read the documents passed in, and create a summary answer for each question passed in.
+  # If the documents are longer than two pages or so, it is reccoemended that you first summaize each document.
+  # docs_description is a single sentance describing what the documents are such as "The texts are a collection of product reviews for a Pickle Ball paddle."
+  
+  def multi_doc(self, docs: list[str], questions: list[str], docs_description: str):
+    self.steps =[]
+    start_time = time.time()
+    answers: str = self._multi_doc(docs, questions, docs_description)
+    end_time = time.time()
+
+    return {
+      'results': answers,
+      'steps': self.steps,
+      'time': round(end_time-start_time,2)
+    }
+
+  def _multi_doc(self, docs: list[str], questions: list[str], docs_description: str):
+    #get answers from each doc for each question.
+    answers = {}
+    prompt2quetion_doc = {}
+    prompts = []
+    max_docs_to_scan = 500
+    
+    #build the queries to be passed into Bedrock
+    for question in questions:
+      for x, doc in enumerate(docs):
+        if x>max_docs_to_scan:
+          break #limit for testing
+        
+        #print ("Asking the LLM to find extract answers from this doc:",doc)
+        questions_prompt = self.get_prompt(docs[doc],"reporter","list", question, "",docs_description)
+        prompt2quetion_doc[questions_prompt] = (question,doc) 
+        prompts.append(questions_prompt)
+        # Add the question prompt as a step in the calculation.
+        self.add_step('Generated question prompt for input document', doc, questions_prompt)
+        
+    if self.debug:
+      print("Starting %s worker threads."%len(prompts))
+
+    prompts_answers = self.ask_claude_threaded(prompts)
+    # save_calls(claude_cache)
+    
+    for question in questions:
+      answers[question] = []    
+    
+    for prompt,answer,total_tokens,output_tokens,request_time,tokens_per_sec,query_start_time in prompts_answers:
+      question,doc = prompt2quetion_doc[prompt]
+      answers[question].append(answer)
+      self.add_step('Generated answer for input document', doc, answer)
+        
+    
+    current_answer_count = len(docs)
+    if self.debug: 
+      print("All documents have been read.  Reducing answers into the final summary...")
+
+    #reduce this down to 5 or less docs for the final summary by combining the individual answers.
+    while current_answer_count > 5:
+      #summarize the answers
+      prompts = []
+      prompts2question = {}
+      
+      max_docs_to_scan = max(min(current_answer_count,8),3)
+      if self.debug: 
+         print("Combining %s chunks.  (Currently there are %s answers to each question.)"%(max_docs_to_scan,current_answer_count))
+
+      for question in questions:
+        #You want chunks of roughly 2K tokens
+        for partial_chunks in self.grab_set_chunks(answers[question],max_docs_to_scan):
+          questions_prompt = self.get_prompt(partial_chunks,"reporter_summary","list", question, " in less than 1000 tokens")
+          prompts.append(questions_prompt)
+          prompts2question[questions_prompt] = question
+      
+      if self.debug:
+        print("Starting %s worker threads."%len(prompts))
+
+      prompts_answers = self.ask_claude_threaded(prompts)
+      # save_calls(claude_cache)
+      
+      for question in questions:
+        answers[question] = []    
+      for prompt,answer,total_tokens,output_tokens,request_time,tokens_per_sec,query_start_time in prompts_answers:
+        answers[prompts2question[prompt]].append(answer)  
+        self.add_step('Generated answer for question prompt', prompts2question[prompt], answer)      
+
+      current_answer_count = len(answers[questions[0]])
+        
+    if self.debug: 
+      print("Creating the final summary for each question.")
+
+    #write the final article:
+    prompts = []
+    prompts2question = {}
+    for question in questions:
+      #print ("Asking the LLM to finalize the answer for this question:",question)
+      questions_prompt = self.get_prompt(answers[question],"reporter_final","narrative", question, "")
+      prompts.append(questions_prompt)
+      prompts2question[questions_prompt] = question
+
+    if self.debug:
+      print("Starting %s worker threads."%len(prompts))
+
+    prompts_answers = self.ask_claude_threaded(prompts)
+    # save_calls(claude_cache)
+    
+    answers = {}
+    for prompt,answer,total_tokens,output_tokens,request_time,tokens_per_sec,query_start_time in prompts_answers:
+      answers[prompts2question[prompt]] = answer
+      self.add_step('Compile answers', prompts2question[prompt], answer) 
+
+    self.add_step('Add answers', 'input is from the questions and input documents', answers) 
+    return answers
+  
+  # Yield successive n-sized chunks from lst.
+  # This is a helper function for the multidoc summarization function.
+  def grab_set_chunks(self, lst, n):
+    for i in range(0, len(lst), n):
+      yield lst[i:i + n]
+
+  def add_step(self, action: str, input: str, result: str):
+    self.steps.append(Step(action=action, input=input, results=result))
+  
